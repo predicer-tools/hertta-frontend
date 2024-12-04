ee266b189a9ebdd4f56c6a21ca5f16c54a4e0b9d
Essi Nousiainen <essi.nousiainen@vtt.fi>
2024-12-04 12:53:18 +0200

    electricity price now ok, added tax to price

--- mock-backend/app/routes.py
+++ mock-backend/app/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
 import requests
+from flask import Blueprint, jsonify, request
+
 from .weather import weather_bp
 from .mock_homeassistant import mock_homeassistant_bp
 
@@ -19,7 +20,13 @@ def get_mock_data():
 
 @main_bp.route('/api/electricity-prices', methods=['GET'])
 def get_electricity_prices():
-    api_url = "https://dashboard.elering.ee/api/nps/price?start=2024-12-04T00%3A00%3A00.000Z&end=2024-12-04T12%3A00%3A00.000Z"
+    start_time = request.args.get('start_time')
+    end_time = request.args.get('end_time')
+
+    if not start_time or not end_time:
+        return jsonify({"error": "start_time and end_time are required parameters"}), 400
+
+    api_url = f"https://dashboard.elering.ee/api/nps/price?start={start_time}&end={end_time}"
     try:
         response = requests.get(api_url)
         response.raise_for_status()
