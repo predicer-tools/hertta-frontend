0862c008a3debd8fa2ca5c44c96b2e554786bf97
Essi Nousiainen <essi.nousiainen@vtt.fi>
2026-06-09 11:10:51 +0300

    added control signals to backend paths

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -17,6 +17,7 @@ module.exports = function(app) {
     '/hertta-health',
     '/start-hourly-optimization',
     '/stop-hourly-optimization',
+    '/control-signals',
     '/weather',
     '/start-weather',
     '/prices',
